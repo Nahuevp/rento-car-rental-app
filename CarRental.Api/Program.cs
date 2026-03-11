485156698b1364dbbda297089bc8cc4641ca46b8
Nahuevp <nviera841@gmail.com>
2026-03-11 03:34:39 -0300

    Use EnsureCreated instead of Migrate

--- CarRental.Api/Program.cs
+++ CarRental.Api/Program.cs
@@ -70,7 +70,7 @@ if (app.Environment.IsDevelopment())
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 app.UseRouting();
